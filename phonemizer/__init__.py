87894a6bb094257f5246ea32ca8da466df4af6e3
Corentin Jemine <corentin.jemine@gmail.com>
2020-10-06 23:48:02 +0200

    Fixed resemble not being in version

--- phonemizer/__init__.py
+++ phonemizer/__init__.py
@@ -14,7 +14,7 @@
 # along with phonologizer. If not, see <http://www.gnu.org/licenses/>.
 """Multilingual text to phones converter"""
 
-__version__ = '2.2.1'
+__version__ = '2.2.1-resemble'
 
 
 try:  # pragma: nocover
